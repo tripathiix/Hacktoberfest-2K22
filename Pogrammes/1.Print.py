6cbe781149b0de80a18a98c84fa087b40d53c0e8
divyarajsinh3315 <114909642+divyarajsinh3315@users.noreply.github.com>
2022-10-03 14:23:39 +0530

    Update 1.Print.py

--- Pogrammes/1.Print.py
+++ Pogrammes/1.Print.py
@@ -1,7 +1,7 @@
 print("hello world")
 print('hello Python')
 print("hello \'Earth\' Jupyter") #use single quote
-print('hello \"Earth\" Python') #use double quote
+print('hello \"Earth\" Django') #use double quote
 print("hello Hulk \nhello iron Man") #new line
 print("hello \\ Marvel") #use backslash
 print("hello \tSpider Man") #use tab
