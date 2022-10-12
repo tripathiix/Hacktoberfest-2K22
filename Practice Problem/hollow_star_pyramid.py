995dbfd9f7d7f4a319621f6f4162e55334aca432
krisprajapati00 <115491018+krisprajapati00@users.noreply.github.com>
2022-10-12 21:32:48 +0530

    Create hollow_star_pyramid.py

---
+++ Practice Problem/hollow_star_pyramid.py
@@ -0,0 +1,14 @@
+# Generating Hollow Pyramid Pattern Using Stars
+
+row = int(input('Enter number of rows required: '))
+
+for i in range(row):
+    for j in range(row-i):
+        print(' ', end='') # printing space required and staying in same line
+    
+    for j in range(2*i+1):
+        if j==0 or j==2*i or i==row-1:
+            print('*',end='')
+        else:
+            print(' ', end='')
+    print() # printing new line
