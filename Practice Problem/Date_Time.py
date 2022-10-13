c3a01147ad7ff34dad7e44c631e8d1256ceb424f
JotangiaTanmay <115491384+JotangiaTanmay@users.noreply.github.com>
2022-10-13 21:46:31 +0530

    Update Date_Time.py

--- Practice Problem/Date_Time.py
+++ Practice Problem/Date_Time.py
@@ -1,2 +1,6 @@
 import time
+import calendar
+
 print(time.ctime()) #FOR EX. (Thu Oct 13 16:01:07 2022)
+print("------------------------------------------------------------------------")
+print(calendar.month(2022, 7))
